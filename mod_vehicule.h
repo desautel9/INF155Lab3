f42e6cebd487e4f39b6e50c8d2f562bfbc816c4f
pierre <18192@DESKTOP-TB3JIHM>
2021-11-15 19:37:12 -0500

    en tete du mod_vehicule.h

---
+++ mod_vehicule.h
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+void vehicule_reset_geometrie(t_vehicule* vehicule);
+/*Réinitialise le polygone du véhicule dont la référence est passée en
+paramètre.Ensuite, la fonction doit définir les coordonnées des points du
+polygone en se basant sur VEHICULE_LONGUEUR et VEHICULE_LARGEUR.Le premier
+point du polygone doit absolument se trouver aux coordonnées(0, 0).*/
+
+
+
+double vehicule_calculer_deplacement(t_vehicule* vehicule, double delta_t);
+/*Calcule et retourne la valeur du déplacement du véhicule(passé par
+	référence) après un temps delta_t en se basant sur sa vitesse.*/
