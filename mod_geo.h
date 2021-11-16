e542d92ff7bdc86ab70f995ffd418513a0168945
David Hill <david.hill.1@ens.etsmtl.ca>
2021-11-15 19:27:27 -0500

    valider

--- mod_geo.h
+++ mod_geo.h
@@ -4,7 +4,7 @@ typedef struct {
 	int x; //abscisse
 	int y; //ordonn�e//
 } t_point;
-//D�finit un polygone qui est un ensemble de points
+//Definit un polygone qui est un ensemble de points
 typedef struct t_polygone {
 	t_point points[GEO_POLYGONE_MAX_PTS]; //Tableau statique de points
 	int nb_points; //Nombre de points dans le tableau
