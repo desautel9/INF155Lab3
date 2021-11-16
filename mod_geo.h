5ced99e42b5c0270c47f077a969995655e015756
desautel9 <odesautels@gmail.com>
2021-11-15 19:20:39 -0500

    Ajout de geo_ajouter_tab_polygone et geo_rotation_point

--- mod_geo.h
+++ mod_geo.h
@@ -9,3 +9,22 @@ typedef struct t_polygone {
 	t_point points[GEO_POLYGONE_MAX_PTS]; //Tableau statique de points
 	int nb_points; //Nombre de points dans le tableau
 } t_polygone;
+
+/*
+Ajoute les nb_pts points se trouvant dans le tableau pts au polygone poly.
+Les points sont ajoutés dans le polygone à partir de la position 0.
+La fonction renvoie une valeur vraie si tous les points du tableau ont pu
+être ajoutés au polygone, ou valeur fausse sinon.
+*/
+int geo_ajouter_tab_polygone(t_polygone* poly, t_point pts[], int nb_pts);
+
+/*
+Reçoit une référence vers un point point et un angle angle. La fonction
+effectue la rotation du point par rapport à l’origine. En supposant que le
+point se trouve aux coordonnées (x,y) et que ses coordonnées après la
+rotation sont (x’, y’), la formule permettant de calculer les nouvelles
+coordonnées est la suivante :
+x’ = x.cos(angle) - y.sin(angle)
+y’ = x.sin(angle) + y.cos(angle) 
+*/
+void geo_rotation_point(t_point* point, double angle);
