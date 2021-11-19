649ac5add2517f56003d089b659b21959529266b
desautl9 <odesautels@gmail.com>
2021-11-19 15:06:54 -0500

    ajout de definitions de mod voie

--- mod_voie.cpp
+++ mod_voie.cpp
@@ -1,1 +1,48 @@
 #include "mod_voie.h"
+
+int voie_changement_voie_possible(const t_voie* nouvelle_voie, const t_vehicule* vehicule)
+{
+	return 0;
+}
+
+void voie_afficher_console(t_voie* voie)
+{
+}
+
+int voie_trouver_vehicule(const t_voie* voie, const t_vehicule* vehicule)
+{
+	return 0;
+}
+
+double voie_dist_vehicule_suivant(const t_voie* voie, const t_vehicule* vehicule)
+{
+	return 0.0;
+}
+
+double voie_dist_vehicule_precedent(const t_voie* voie, const t_vehicule* vehicule)
+{
+	return 0.0;
+}
+
+double voie_vitesse_cible_vehicule_precedent(t_voie* voie, t_vehicule* vehicule)
+{
+	return 0.0;
+}
+
+double voie_vitesse_vehicule_suivant(t_voie* voie, t_vehicule* vehicule)
+{
+	return 0.0;
+}
+
+void voie_avance_vehicule(t_voie* voie, t_vehicule* vehicule, double distance)
+{
+}
+
+void voie_avancer_vehicule(t_voie* voie, t_vehicule* vehicule, double distance)
+{
+}
+
+int voie_nb_vehicules_sous_vitesse_cible(t_voie* voie)
+{
+	return 0;
+}
