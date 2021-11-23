ba570359cd873961c0d18dea130bf53b43995191
pierre <18192@DESKTOP-TB3JIHM>
2021-11-22 19:50:20 -0500

    ajout fonction pierre semaine 1

--- mod_vehicule.cpp
+++ mod_vehicule.cpp
@@ -12,9 +12,27 @@ void vehicule_free(t_vehicule* vehicule)
 }
 
 void vehicule_reset_geometrie(t_vehicule* vehicule)
-{
+{ 
+	for (int i = 0; i <= vehicule->geometrie->nb_points; i++)
+	{ 
+		vehicule->geometrie->points[i]->x = 0;
+		vehicule->geometrie->points[i]->y = 0;
+		
+	}
 
-	//TODO: Compléter cette fonction
+	vehicule->geometrie->points[0]->x = 0;
+	vehicule->geometrie->points[0]->y = 0;
+
+	vehicule->geometrie->points[1]->x = VEHICULE_LONGUEUR;
+	vehicule->geometrie->points[1]->y = 0;
+	
+	vehicule->geometrie->points[2]->x = 0;
+	vehicule->geometrie->points[2]->y = VEHICULE_LARGEUR;
+
+	vehicule->geometrie->points[3]->x = VEHICULE_LONGUEUR;
+	vehicule->geometrie->points[3]->y = VEHICULE_LARGEUR;
+
+	
 }
 
 void vehicule_decision(t_vehicule* vehicule, const t_route* route, double delai_sec, int faciliter_depassement)
@@ -28,8 +46,8 @@ int vehicule_changer_voie(t_vehicule* vehicule, const t_route* route, int nouvel
 
 double vehicule_calculer_deplacement(t_vehicule* vehicule, double delta_t_sec)
 {
-	vehicule->position = vehicule.position + (vehicule.vitesse * delta_t_sec); //attribue la nouvelle position en fonction de la vitesse 
+	double nouvelle_position =  (vehicule.vitesse * delta_t_sec); //calcul variation position
+	
 
-	//TODO: Compléter cette fonction, la valeur de retournee ici est juste pour la compilation 
-	return 0;
+	return nouvelle_position;
 }
