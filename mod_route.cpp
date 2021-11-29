2cb81b4eb96546277e94262758a06111360a260f
David Hill <david.hill.1@ens.etsmtl.ca>
2021-11-29 15:13:30 -0500

    route init

--- mod_route.cpp
+++ mod_route.cpp
@@ -21,13 +21,6 @@ t_route* route_init(int nb_voies, double vitesse_min, double vitesse_max, double
 		exit(EXIT_FAILURE);
 	}
 
-	route->voies = (t_voie**)malloc(sizeof(t_voie*) * ROUTE_MAX_VOIES); //Alloue un emplacement pour un tableau de t_voie
-	if (route->voies == NULL)
-	{
-		printf("Erreur d'allocation!\n");
-		exit(EXIT_FAILURE);
-	}
-
 	route->nb_voies = nb_voies;
 	route->vitesse_min = vitesse_min;
 	route->vitesse_max = vitesse_max;
@@ -35,7 +28,7 @@ t_route* route_init(int nb_voies, double vitesse_min, double vitesse_max, double
 	for (int i = 0; i < nb_voies; i++)
 	{
 		t_voie* voies = voie_init(longueur_visible, max_vehicules_voie);
-		route->voies = voies;
+		route->voies[i] = voies;
 	}
 
 	return route;
