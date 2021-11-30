2c2ac71401272c83e03500c751e66c8b862c2ffa
pierre <18192@DESKTOP-TB3JIHM>
2021-11-29 20:53:41 -0500

    fontion vehicule lent

--- mod_voie.cpp
+++ mod_voie.cpp
@@ -211,16 +211,16 @@ void voie_avancer_vehicule(t_voie* voie, t_vehicule* vehicule, double distance)
 
 int voie_nb_vehicules_sous_vitesse_cible(t_voie* voie)
 {
-	int i, j;
+	int i, nb_vehicule_lent=0;
 
 	for (i = 0; i <= voie->nb_vehicule; ++i)
 	{
-		if (route->voies->)
-			++j;
+		if (voie->vehicules[i]->vitesse < (voie->vehicules[i]->vitesse_cible*0.9))// vitesse vehicule inferieur de 10% de vitesse cible 
+			++nb_vehicule_lent;
 	}
 	
 
-	return j;
+	return nb_vehicule_lent;// retourne le total de vehicule lent sur la voie 
 }
 
 
