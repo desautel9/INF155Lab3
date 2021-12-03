b7c27ef75999a38547f557079b917ced73f1a440
desautl9 <odesautels@gmail.com>
2021-12-03 15:10:24 -0500

    modif

--- mod_vehicule.c
+++
@@ -1,19 +0,0 @@
-//
-// Créer par David Hill, Olivier Deshautels, Pierre Rayeroux
-// Date remise : 2021-12-04
-//
-
-#include "mod_vehicule.h"
-#define _CRT_SECURE_NO_WARNINGS
-
-
-
-
-
-void vehicule_reset_geometrie(t_vehicule* vehicule)
-/*Réinitialise le polygone du véhicule dont la référence est passée en
-paramètre.Ensuite, la fonction doit définir les coordonnées des points du
-polygone en se basant sur VEHICULE_LONGUEUR et VEHICULE_LARGEUR.Le premier
-point du polygone doit absolument se trouver aux coordonnées(0, 0).*/{}double vehicule_calculer_deplacement(t_vehicule* vehicule, double delta_t)
-/*Calcule et retourne la valeur du déplacement du véhicule(passé par
-	référence) après un temps delta_t en se basant sur sa vitesse.*/{}
